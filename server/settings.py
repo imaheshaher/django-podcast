f5da84eff1482ecb370c8b6b44571d2da0b88390
imaheshaher <maheshaher035@gmail.com>
2023-04-27 22:02:34 +0530

    pgsql setup

--- server/settings.py
+++ server/settings.py
@@ -71,25 +71,37 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'server.wsgi.application'
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, 'scrap.db'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(PROJECT_DIR, 'scrap.db'),
+#     }
+# }
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scraper_data',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
+
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'scraper_data',
-#         'USER': 'postgres',
-#         'PASSWORD': 'root',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432'
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME': 'scrap',
+#         'USER': 'root',
+#         'PASSWORD': 'Mahesh@123',
+#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
 #     }
+
 # }
 
 
